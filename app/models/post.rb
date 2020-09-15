bb1487077f8ed8f1b2bd601c5b3f9ab2aa6abfe8
gautham <gauthamniranjan7@gmail.com>
2020-09-15 21:34:53 +0530

    initial1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  validates_presence_of :title
+    validates_presence_of :body
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_064412) do
+ActiveRecord::Schema.define(version: 2020_09_15_075927) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
