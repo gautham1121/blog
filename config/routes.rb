7b550c851ea8594807ff18510bcb49dc1f70d450
gautham <gauthamniranjan7@gmail.com>
2020-09-17 23:39:29 +0530

    initial2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :comments
-  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
